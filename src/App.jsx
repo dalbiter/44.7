9011d18f5f55ed5225a89988e5114f6ec7c23491
danx <rushn24@gmail.com>
2024-07-15 22:05:46 -0400

    dice roll game

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Dice from './Dice'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="App">
+      <Dice />
+      <Dice numDice={2} title="Craps" maxVal={6} />
+    </div>
   )
 }
 
